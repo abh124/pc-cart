924b8b17d219e6009cf6886ac97bf547887d361e
hsaynchreif <64301182+abh124@users.noreply.github.com>
2025-12-28 13:28:35 +0200

    fc

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' })); // Increased limit for base64 images
