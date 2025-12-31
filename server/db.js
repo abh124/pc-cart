052c26dab56dda0c97897f473a0b9dc8c0af2969
hsaynchreif <64301182+abh124@users.noreply.github.com>
2025-12-31 16:32:13 +0200

    prepare backend for cloud db

--- server/db.js
+++ server/db.js
@@ -1,10 +1,15 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '', // Default XAMPP password is empty
-  database: 'pccart'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
